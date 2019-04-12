9e77a2f6be73a5697a04caa1500950ab1e8b1ddb
Matheusrsm <matheusrsilvam@gmail.com>
2019-04-12 11:31:41 -0300

    Update

--- src/doghost/Dog.java
+++ src/doghost/Dog.java
@@ -7,104 +7,130 @@ import java.util.Objects;
  */
 public class Dog {
 
-    /**
-     * Nome do dog em questao.
-     */
-    private String nome;
+	/**
+	 * Nome do dog em questao.
+	 */
+	private String nome;
 
-    /**
-     * Nome do tutor/dono do dog.
-     */
-    private String tutor;
+	/**
+	 * Nome do tutor/dono do dog.
+	 */
+	private String tutor;
 
-    /**
-     * Quanto de racao o dog consome diariamente.
-     */
-    private int consumoRacao;
+	/**
+	 * Quanto de racao o dog consome diariamente.
+	 */
+	private int consumoRacao;
 
-    /**
-     * Restricao que pode tornar a racao mais cara. (i.e. obesos, alergicos, celiacos, veganos, etc).
-     */
-    private String restricao;
+	/**
+	 * Restricao que pode tornar a racao mais cara. (i.e. obesos, alergicos,
+	 * celiacos, veganos, etc).
+	 */
+	private String restricao;
 
-    /**
-     * Construtor de um dog que nao possui restricoes quanto a alimentacao.
-     *
-     * @param nome         Nome do dog.
-     * @param tutor        Nome do tutor/dono do dog.
-     * @param consumoRacao Quanto de racao o dog consome diariamente.
-     */
-    public Dog(String nome, String tutor, int consumoRacao) {
-        validarEntradas(nome, tutor, consumoRacao);
+	/**
+	 * Construtor de um dog que nao possui restricoes quanto a alimentacao.
+	 *
+	 * @param nome         Nome do dog.
+	 * @param tutor        Nome do tutor/dono do dog.
+	 * @param consumoRacao Quanto de racao o dog consome diariamente.
+	 */
+	public Dog(String nome, String tutor, int consumoRacao) {
+		validarEntradas(nome, tutor, consumoRacao);
 
-        this.nome = nome;
-        this.tutor = tutor;
-        this.consumoRacao = consumoRacao;
-    }
+		this.nome = nome;
+		this.tutor = tutor;
+		this.consumoRacao = consumoRacao;
+	}
 
-    /**
-     * Construtor de um dog que possui restricoes quanto a sua alimentacao.
-     *
-     * @param nome         Nome do dog.
-     * @param tutor        Nome do tutor/dono do dog.
-     * @param consumoRacao Quanto de racao o dog consome diariamente.
-     * @param restricao    Restricao quanto a alimentacao do dog. (i.e. obesos, alergicos, celiacos, veganos, etc)
-     */
-    public Dog(String nome, String tutor, int consumoRacao, String restricao) {
-        validarEntradas(nome, tutor, consumoRacao);
+	/**
+	 * Construtor de um dog que possui restricoes quanto a sua alimentacao.
+	 *
+	 * @param nome         Nome do dog.
+	 * @param tutor        Nome do tutor/dono do dog.
+	 * @param consumoRacao Quanto de racao o dog consome diariamente.
+	 * @param restricao    Restricao quanto a alimentacao do dog. (i.e. obesos,
+	 *                     alergicos, celiacos, veganos, etc)
+	 */
+	public Dog(String nome, String tutor, int consumoRacao, String restricao) {
+		validarEntradas(nome, tutor, consumoRacao);
 
-        this.nome = nome;
-        this.tutor = tutor;
-        this.consumoRacao = consumoRacao;
-        this.restricao = restricao;
-        this.consumoRacao *= 2;
-    }
+		this.nome = nome;
+		this.tutor = tutor;
+		this.consumoRacao = consumoRacao;
+		this.restricao = restricao;
+		this.consumoRacao *= 2;
+	}
 
-    /**
-     * Getter do consumo total de racao.
-     *
-     * @return Retorna o atributo {@code consumoRacao}
-     */
-    public int getConsumoRacao() {
-        return consumoRacao;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    @Override
-    public String toString() {
-        return "Dog [name=" + nome + ", tutor=" + tutor + "]";
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Dog dog = (Dog) o;
-        return nome.equals(dog.nome) && tutor.equals(dog.tutor);
-    }
+	public String getTutor() {
+		return tutor;
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(nome, tutor);
-    }
+	public void setTutor(String tutor) {
+		this.tutor = tutor;
+	}
 
-    /**
-     * Metodo auxiliar para validar as entradas dos construtores.
-     *
-     * @param nome         Nome do dog.
-     * @param tutor        Nome do tutor/dono do dog.
-     * @param consumoRacao Quanto de racao o dog consome diariamente.
-     */
-    private void validarEntradas(String nome, String tutor, int consumoRacao) {
-        if (nome == null)
-            throw new NullPointerException("Nome nulo!");
-        if (nome.trim().equals(""))
-            throw new IllegalArgumentException("Nome vazio!");
-        if (tutor == null)
-            throw new NullPointerException("Nome de tutor nulo!");
-        if (tutor.trim().equals(""))
-            throw new IllegalArgumentException("Nome de tutor vazio!");
-        if (consumoRacao <= 0)
-            throw new IllegalArgumentException("Consumo invalido!");
-    }
+	public String getRestricao() {
+		return restricao;
+	}
 
+	public void setRestricao(String restricao) {
+		this.restricao = restricao;
+	}
+
+	public int getConsumoRacao() {
+		return consumoRacao;
+	}
+
+	public void setConsumoRacao(int consumoRacao) {
+		this.consumoRacao = consumoRacao;
+	}
+
+	@Override
+	public String toString() {
+		return "Dog [name=" + nome + ", tutor=" + tutor + "]";
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Dog dog = (Dog) o;
+		return nome.equals(dog.nome) && tutor.equals(dog.tutor);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nome, tutor);
+	}
+
+	/**
+	 * Metodo auxiliar para validar as entradas dos construtores.
+	 *
+	 * @param nome         Nome do dog.
+	 * @param tutor        Nome do tutor/dono do dog.
+	 * @param consumoRacao Quanto de racao o dog consome diariamente.
+	 */
+	private void validarEntradas(String nome, String tutor, int consumoRacao) {
+		if (nome == null)
+			throw new NullPointerException("Nome nulo!");
+		if (nome.trim().equals(""))
+			throw new IllegalArgumentException("Nome vazio!");
+		if (tutor == null)
+			throw new NullPointerException("Nome de tutor nulo!");
+		if (tutor.trim().equals(""))
+			throw new IllegalArgumentException("Nome de tutor vazio!");
+		if (consumoRacao <= 0)
+			throw new IllegalArgumentException("Consumo invalido!");
+	}
 }
--- src/doghost/DogHost.java
+++ src/doghost/DogHost.java
@@ -5,142 +5,177 @@ package doghost;
  */
 public class DogHost {
 
-    /**
-     * Nome do anfitriao.
-     */
-    private String nome;
+	/**
+	 * Nome do anfitriao.
+	 */
+	private String nome;
 
-    /**
-     * Maximo de racao que o anfitriao se disponibiliza a comprar.
-     */
-    private int maximoRacao;
+	/**
+	 * Maximo de racao que o anfitriao se disponibiliza a comprar.
+	 */
+	private int maximoRacao;
 
-    /**
-     * Array que armazena os doguinhos hospedados.
-     */
-    private Dog[] dogs;
+	/**
+	 * Array que armazena os doguinhos hospedados.
+	 */
+	private Dog[] dogs;
 
-    /**
-     * Contados que armazena o indice do ultimo dog.
-     */
-    private int indiceDogs;
+	/**
+	 * Contados que armazena o indice do ultimo dog.
+	 */
+	private int indiceDogs;
 
-    /**
-     * Construtor unico do anfitriao.
-     *
-     * @param nome        Nome do anfitriao.
-     * @param maximoDogs  Maximo de doguinhos que o anfitriao se disponibiliza para cuidar.
-     * @param maximoRacao Maximo de racao que o anfitriao se disponibiliza a comprar.
-     */
-    public DogHost(String nome, int maximoDogs, int maximoRacao) {
-        this.nome = nome;
-        this.dogs = new Dog[maximoDogs];
-        this.maximoRacao = maximoRacao;
-        this.indiceDogs = 0;
-    }
+	/**
+	 * Construtor unico do anfitriao.
+	 *
+	 * @param nome        Nome do anfitriao.
+	 * @param maximoDogs  Maximo de doguinhos que o anfitriao se disponibiliza para
+	 *                    cuidar.
+	 * @param maximoRacao Maximo de racao que o anfitriao se disponibiliza a
+	 *                    comprar.
+	 */
+	public DogHost(String nome, int maximoDogs, int maximoRacao) {
+		this.nome = nome;
+		this.dogs = new Dog[maximoDogs];
+		this.maximoRacao = maximoRacao;
+		this.indiceDogs = 0;
+	}
 
-    /**
-     * Metodo responsavel pela hospedagem de um doguinho.
-     *
-     * @param dog Doguinho a ser adicionado.
-     * @return true ou false dependendo do resultado da hospedagem. (Considerando o maximo de Racao e
-     * o maximo de dogs.)
-     */
-    public boolean adicionaDog(Dog dog) {
+	/**
+	 * Metodo responsavel pela hospedagem de um doguinho.
+	 *
+	 * @param dog Doguinho a ser adicionado.
+	 * @return true ou false dependendo do resultado da hospedagem. (Considerando o
+	 *         maximo de Racao e o maximo de dogs.)
+	 */
+	public boolean adicionaDog(Dog dog) {
 
-        boolean result = false;
+		boolean result = false;
 
-        if (dog.getConsumoRacao() + totalRacao() < maximoRacao && indiceDogs < dogs.length) {
-            dogs[indiceDogs] = dog;
-            indiceDogs++;
-            result = true;
-        }
+		if (dog.getConsumoRacao() + totalRacao() < maximoRacao && indiceDogs < dogs.length) {
+			dogs[indiceDogs] = dog;
+			indiceDogs++;
+			result = true;
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    /**
-     * Metodo responsavel por listar todos os dogs hospedados pelo anfitriao pela ordem de cadastro.
-     *
-     * @return o nome do anfitriao seguido do .toString() de cada um dos doguinhos, separados por uma
-     * quebra de linha.
-     */
-    public String listaDogs() {
+	/**
+	 * Metodo responsavel por listar todos os dogs hospedados pelo anfitriao pela
+	 * ordem de cadastro.
+	 *
+	 * @return o nome do anfitriao seguido do .toString() de cada um dos doguinhos,
+	 *         separados por uma quebra de linha.
+	 */
+	public String listaDogs() {
 
-        String result = nome + ":" + System.lineSeparator();
+		String result = nome + ":" + System.lineSeparator();
 
-        for (int i = 0; i < dogs.length; i++) {
-            result += dogs[i].toString();
-        }
+		for (int i = 0; i < dogs.length; i++) {
+			result += dogs[i].toString();
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    /**
-     * Consulta o valor da hospedagem de um doguinho por uma determinada quantia de tempo. Lança uma
-     * excecao caso ele nao esteja cadastrado.
-     *
-     * @param toto Dog a ser avaliado.
-     * @param dias Quantos dias ele estara hospedado.
-     * @return valor (em reais) do custo da hospedagem.
-     */
-    public double consultaValorHospedagem(Dog toto, int dias) {
-        if (!contains(toto))
-            throw new IllegalArgumentException("Dog não cadastrado.");
+	/**
+	 * Consulta o valor da hospedagem de um doguinho por uma determinada quantia de
+	 * tempo. Lança uma excecao caso ele nao esteja cadastrado.
+	 *
+	 * @param toto Dog a ser avaliado.
+	 * @param dias Quantos dias ele estara hospedado.
+	 * @return valor (em reais) do custo da hospedagem.
+	 */
+	public double consultaValorHospedagem(Dog toto, int dias) {
+		if (!contains(toto))
+			throw new IllegalArgumentException("Dog não cadastrado.");
 
-        return consumoDiario(toto) * dias;
-    }
+		return consumoDiario(toto) * dias;
+	}
 
-    /**
-     * Metodo auxiliar que checa se um determinado doguinho esta cadastrado.
-     *
-     * @param toto Dog desejado.
-     * @return valor booleano dependendo do cadastro do doguinho desejado.
-     */
-    private boolean contains(Dog toto) {
-        boolean result = false;
-        for (int i = 0; i < dogs.length; i++) {
-            if (dogs[i].equals(toto)) {
-                result = true;
-                break;
-            }
-        }
-        return result;
-    }
+	/**
+	 * Metodo auxiliar que checa se um determinado doguinho esta cadastrado.
+	 *
+	 * @param toto Dog desejado.
+	 * @return valor booleano dependendo do cadastro do doguinho desejado.
+	 */
+	private boolean contains(Dog toto) {
+		boolean result = false;
+		for (int i = 0; i < dogs.length; i++) {
+			if (dogs[i].equals(toto)) {
+				result = true;
+				break;
+			}
+		}
+		return result;
+	}
 
-    /**
-     * Total de racao consumida diariamente pelos dogs hospedados.
-     *
-     * @return Consumo diario de racao.
-     */
-    public int totalRacao() {
-        int result = 0;
-        for (int i = 0; i < dogs.length; i++) {
-            result += dogs[i].getConsumoRacao();
-        }
-        return result;
-    }
+	/**
+	 * Total de racao consumida diariamente pelos dogs hospedados.
+	 *
+	 * @return Consumo diario de racao.
+	 */
+	public int totalRacao() {
+		int result = 0;
+		for (int i = 0; i < dogs.length; i++) {
+			result += dogs[i].getConsumoRacao();
+		}
+		return result;
+	}
 
-    /**
-     * Gasto total diario com racao.
-     *
-     * @return Valor (em reais) gasto.
-     */
-    public double totalValorDiario() {
-        double result = 0;
-        for (int i = 0; i < dogs.length; i++) {
-            result += consumoDiario(dogs[i]);
-        }
-        return result;
-    }
+	/**
+	 * Gasto total diario com racao.
+	 *
+	 * @return Valor (em reais) gasto.
+	 */
+	public double totalValorDiario() {
+		double result = 0;
+		for (int i = 0; i < dogs.length; i++) {
+			result += consumoDiario(dogs[i]);
+		}
+		return result;
+	}
 
-    /**
-     * Metodo auxiliar que retorna o consumo de um determinado dog.
-     *
-     * @param dog Doguinho desejado.
-     * @return Consumo (em reais).
-     */
-    private double consumoDiario(Dog dog) {
-        return dog.getConsumoRacao() * 0.4;
-    }
+	/**
+	 * Metodo auxiliar que retorna o consumo de um determinado dog.
+	 *
+	 * @param dog Doguinho desejado.
+	 * @return Consumo (em reais).
+	 */
+	private double consumoDiario(Dog dog) {
+		return dog.getConsumoRacao() * 0.4;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getMaximoRacao() {
+		return maximoRacao;
+	}
+
+	public void setMaximoRacao(int maximoRacao) {
+		this.maximoRacao = maximoRacao;
+	}
+
+	public Dog[] getDogs() {
+		return dogs;
+	}
+
+	public void setDogs(Dog[] dogs) {
+		this.dogs = dogs;
+	}
+
+	public int getIndiceDogs() {
+		return indiceDogs;
+	}
+
+	public void setIndiceDogs(int indiceDogs) {
+		this.indiceDogs = indiceDogs;
+	}
 }
