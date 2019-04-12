28a083014cd49a900431d22b69dea16848ee6252
HenryFilho <henry.filho@ccc.ufcg.edu.br>
2019-04-11 23:38:56 -0300

    Javadoc

--- src/doghost/Dog.java
+++ src/doghost/Dog.java
@@ -2,25 +2,69 @@ package doghost;
 
 import java.util.Objects;
 
+/**
+ * Classe que representa os dogs que podem ser hospedados pelos doghosts.
+ */
 public class Dog {
 
+    /**
+     * Nome do dog em questao.
+     */
     private String nome;
+
+    /**
+     * Nome do tutor/dono do dog.
+     */
     private String tutor;
+
+    /**
+     * Quanto de racao o dog consome diariamente.
+     */
     private int consumoRacao;
 
-    public Dog(String nome, String tutor, int consumoRacao) {
-        this(nome, tutor, consumoRacao, false);
-    }
+    /**
+     * Restricao que pode tornar a racao mais cara. (i.e. obesos, alergicos, celiacos, veganos, etc).
+     */
+    private String restricao;
 
-    public Dog(String nome, String tutor, int consumoRacao, boolean restricao) {
+    /**
+     * Construtor de um dog que nao possui restricoes quanto a alimentacao.
+     *
+     * @param nome         Nome do dog.
+     * @param tutor        Nome do tutor/dono do dog.
+     * @param consumoRacao Quanto de racao o dog consome diariamente.
+     */
+    public Dog(String nome, String tutor, int consumoRacao) {
+        validarEntradas(nome, tutor, consumoRacao);
+
         this.nome = nome;
         this.tutor = tutor;
         this.consumoRacao = consumoRacao;
-        if (restricao) {
-            this.consumoRacao *= 2;
-        }
     }
 
+    /**
+     * Construtor de um dog que possui restricoes quanto a sua alimentacao.
+     *
+     * @param nome         Nome do dog.
+     * @param tutor        Nome do tutor/dono do dog.
+     * @param consumoRacao Quanto de racao o dog consome diariamente.
+     * @param restricao    Restricao quanto a alimentacao do dog. (i.e. obesos, alergicos, celiacos, veganos, etc)
+     */
+    public Dog(String nome, String tutor, int consumoRacao, String restricao) {
+        validarEntradas(nome, tutor, consumoRacao);
+
+        this.nome = nome;
+        this.tutor = tutor;
+        this.consumoRacao = consumoRacao;
+        this.restricao = restricao;
+        this.consumoRacao *= 2;
+    }
+
+    /**
+     * Getter do consumo total de racao.
+     *
+     * @return Retorna o atributo {@code consumoRacao}
+     */
     public int getConsumoRacao() {
         return consumoRacao;
     }
@@ -42,4 +86,25 @@ public class Dog {
     public int hashCode() {
         return Objects.hash(nome, tutor);
     }
+
+    /**
+     * Metodo auxiliar para validar as entradas dos construtores.
+     *
+     * @param nome         Nome do dog.
+     * @param tutor        Nome do tutor/dono do dog.
+     * @param consumoRacao Quanto de racao o dog consome diariamente.
+     */
+    private void validarEntradas(String nome, String tutor, int consumoRacao) {
+        if (nome == null)
+            throw new NullPointerException("Nome nulo!");
+        if (nome.trim().equals(""))
+            throw new IllegalArgumentException("Nome vazio!");
+        if (tutor == null)
+            throw new NullPointerException("Nome de tutor nulo!");
+        if (tutor.trim().equals(""))
+            throw new IllegalArgumentException("Nome de tutor vazio!");
+        if (consumoRacao <= 0)
+            throw new IllegalArgumentException("Consumo invalido!");
+    }
+
 }
--- src/doghost/DogHost.java
+++ src/doghost/DogHost.java
@@ -1,12 +1,37 @@
 package doghost;
 
+/**
+ * Classe que representa o anfitriao que se propôs a hospedar os cachorros.
+ */
 public class DogHost {
 
+    /**
+     * Nome do anfitriao.
+     */
     private String nome;
+
+    /**
+     * Maximo de racao que o anfitriao se disponibiliza a comprar.
+     */
     private int maximoRacao;
+
+    /**
+     * Array que armazena os doguinhos hospedados.
+     */
     private Dog[] dogs;
+
+    /**
+     * Contados que armazena o indice do ultimo dog.
+     */
     private int indiceDogs;
 
+    /**
+     * Construtor unico do anfitriao.
+     *
+     * @param nome        Nome do anfitriao.
+     * @param maximoDogs  Maximo de doguinhos que o anfitriao se disponibiliza para cuidar.
+     * @param maximoRacao Maximo de racao que o anfitriao se disponibiliza a comprar.
+     */
     public DogHost(String nome, int maximoDogs, int maximoRacao) {
         this.nome = nome;
         this.dogs = new Dog[maximoDogs];
@@ -14,12 +39,19 @@ public class DogHost {
         this.indiceDogs = 0;
     }
 
-    public boolean adicionaDog(Dog d) {
+    /**
+     * Metodo responsavel pela hospedagem de um doguinho.
+     *
+     * @param dog Doguinho a ser adicionado.
+     * @return true ou false dependendo do resultado da hospedagem. (Considerando o maximo de Racao e
+     * o maximo de dogs.)
+     */
+    public boolean adicionaDog(Dog dog) {
 
         boolean result = false;
 
-        if (d.getConsumoRacao() + totalRacao() < maximoRacao && indiceDogs < dogs.length) {
-            dogs[indiceDogs] = d;
+        if (dog.getConsumoRacao() + totalRacao() < maximoRacao && indiceDogs < dogs.length) {
+            dogs[indiceDogs] = dog;
             indiceDogs++;
             result = true;
         }
@@ -27,9 +59,15 @@ public class DogHost {
         return result;
     }
 
+    /**
+     * Metodo responsavel por listar todos os dogs hospedados pelo anfitriao pela ordem de cadastro.
+     *
+     * @return o nome do anfitriao seguido do .toString() de cada um dos doguinhos, separados por uma
+     * quebra de linha.
+     */
     public String listaDogs() {
 
-        String result = nome + ":\n";
+        String result = nome + ":" + System.lineSeparator();
 
         for (int i = 0; i < dogs.length; i++) {
             result += dogs[i].toString();
@@ -38,6 +76,14 @@ public class DogHost {
         return result;
     }
 
+    /**
+     * Consulta o valor da hospedagem de um doguinho por uma determinada quantia de tempo. Lança uma
+     * excecao caso ele nao esteja cadastrado.
+     *
+     * @param toto Dog a ser avaliado.
+     * @param dias Quantos dias ele estara hospedado.
+     * @return valor (em reais) do custo da hospedagem.
+     */
     public double consultaValorHospedagem(Dog toto, int dias) {
         if (!contains(toto))
             throw new IllegalArgumentException("Dog não cadastrado.");
@@ -45,6 +91,12 @@ public class DogHost {
         return consumoDiario(toto) * dias;
     }
 
+    /**
+     * Metodo auxiliar que checa se um determinado doguinho esta cadastrado.
+     *
+     * @param toto Dog desejado.
+     * @return valor booleano dependendo do cadastro do doguinho desejado.
+     */
     private boolean contains(Dog toto) {
         boolean result = false;
         for (int i = 0; i < dogs.length; i++) {
@@ -56,6 +108,11 @@ public class DogHost {
         return result;
     }
 
+    /**
+     * Total de racao consumida diariamente pelos dogs hospedados.
+     *
+     * @return Consumo diario de racao.
+     */
     public int totalRacao() {
         int result = 0;
         for (int i = 0; i < dogs.length; i++) {
@@ -64,6 +121,11 @@ public class DogHost {
         return result;
     }
 
+    /**
+     * Gasto total diario com racao.
+     *
+     * @return Valor (em reais) gasto.
+     */
     public double totalValorDiario() {
         double result = 0;
         for (int i = 0; i < dogs.length; i++) {
@@ -72,6 +134,12 @@ public class DogHost {
         return result;
     }
 
+    /**
+     * Metodo auxiliar que retorna o consumo de um determinado dog.
+     *
+     * @param dog Doguinho desejado.
+     * @return Consumo (em reais).
+     */
     private double consumoDiario(Dog dog) {
         return dog.getConsumoRacao() * 0.4;
     }
